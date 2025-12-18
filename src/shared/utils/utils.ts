9d628993bf8523abf20d8d45310f4d9d552829a5
nirjla <shakyanirjala6@gmail.com>
2025-12-18 06:57:05 +0545

    feat: add utility function to calculate the date one week prior from a given start date

---
+++ src/shared/utils/utils.ts
@@ -0,0 +1,8 @@
+export const getLastWeekFromStartDate = (startDate: string): string => {
+      const lastWeek = new Date();
+      // Subtract 7 days worth of milliseconds (1000ms * 60s * 60m * 24h * 7d)
+      const start = new Date(startDate);
+      lastWeek.setDate(start.getDate() - 7);
+      return lastWeek.toISOString().split('T')[0];
+
+}
