cc15a84be90d0c28d47f6ea1d414b6f7c2b0bf26
nirjla <shakyanirjala6@gmail.com>
2025-12-28 12:39:40 +0545

    refactor: implement dynamic import for octokit/graphql to support CommonJS module format and ensure compatibility

--- src/shared/config/connect-github.ts
+++ src/shared/config/connect-github.ts
@@ -1,13 +1,19 @@
-import { graphql } from "@octokit/graphql";
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
+
+type GraphQLClient = typeof import("@octokit/graphql").graphql;
+
+// NOTE: i'm really sick of this issue, it was taking me many hours just to build in the vercel , currently this version of octokit/graphql is a whole esm module, so i want to downgrade it to a cjs module, so i use dynamic import to import the module and use the graphql function from it
+
 @Injectable()
-export class ConnectGitHub {
-      private client: typeof graphql;
+export class ConnectGitHub implements OnModuleInit {
+      private client!: GraphQLClient;
       private githubToken = process.env.GITHUB_PAT_TOKEN;
-      constructor() {
+
+      async onModuleInit() {
             if (!this.githubToken) {
                   throw new Error("GITHUB_PAT_TOKEN is not defined in environment variables.");
             }
+            const { graphql } = await import("@octokit/graphql");
             this.client = graphql.defaults({
                   headers: {
                         authorization: `token ${this.githubToken}`,
